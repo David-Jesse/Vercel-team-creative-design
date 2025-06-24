55e6423260a1c918cfe87c8c85944e434ad0a713
David-Jesse <djesse351@gmail.com>
2025-06-24 22:45:02 +0100

    added the teams section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import HeroSection from "./components/sections/hero/HeroSection";
 import ResourcesSection from "./components/sections/resource/ResourcesSection";
-
+import TeamSection from "./components/sections/team/TeamSection";
 
 function App() {
   return (
     <div className="app">
       <HeroSection />
       <ResourcesSection />
+      <TeamSection />
     </div>
   );
 }
